7ce114c361dd79af662340f00597cabe2e634083
alvarotabora <41582433+alvarotabora@users.noreply.github.com>
2019-08-07 15:50:47 -0600

    Carga Inicial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './bootstrap.min.css';
+import Header from './components/Header';
+import NuevaCita from './components/NuevaCita';
+import ListaCitas from './components/ListaCitas';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component
+{
+  state = {
+    citas: []
+  }
+
+  //Cuando la aplicacniion cargar
+  componentDidMount()
+  {
+    const citasLS = localStorage.getItem('citas');
+
+    if (citasLS)
+    {
+      this.setState({ citas: JSON.parse(citasLS) });
+    }
+  }
+  
+  //Cuando eliminamos o agregamos
+  componentDidUpdate()
+  {
+    localStorage.setItem('citas', JSON.stringify(this.state.citas));
+  }
+
+  crearNuevaCita = datos =>
+  {
+    //Copiar State Actual
+    const citas = [...this.state.citas, datos];
+
+    //Agregar el nuevo State
+    this.setState({ citas });
+  }
+
+  //Elimina citas del state
+  eliminarCita = id =>
+  {
+    //Tomar copia del state
+    const citasActuales = [...this.state.citas];
+
+    //Utilizar Filter para sacar el id
+    const citas = citasActuales.filter(cita => cita.id !== id);
+
+    //Actualziar el state
+    this.setState({ citas });
+  }
+
+  render()
+  {
+    return (
+      <div className="container">
+        <Header
+          titulo='Administrador pacientes Veterinaria' />
+        
+        <div className="row">
+          <div className="col-md-10 mx-auto">
+            <NuevaCita
+              crearNuevaCita={this.crearNuevaCita}
+            />
+
+            <div className="mt-5 col-md-10 mx-auto">
+              <ListaCitas
+                citas={this.state.citas}
+                eliminarCita={this.eliminarCita}/>
+            </div>
+          </div>
+        </div>
+      </div>);
+  }
 }
-
+ 
 export default App;
---
+++ src/components/ListaCitas.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Cita from './Cita';
+
+const ListaCitas = ({ citas, eliminarCita }) =>
+{
+    // Imprimir mensaje sino hay citas
+    const mensaje = Object.keys(citas).length === 0 ? 'No hay citas' : 'Administra las citas Aqui';
+
+    return (
+        (
+            <div className="card mt-2 py-5">
+                <div className="card-body">
+                    <h2 className="card-title text-center">{mensaje}</h2>
+        
+                    <div className="lista-citas">
+                        {citas.map(cita =>
+                            (
+                                <Cita
+                                    key={cita.id}
+                                    cita={cita}
+                                    eliminarCita={eliminarCita}
+                                />
+                            ))}
+                    </div>
+                </div>
+            </div>
+        )
+    )
+}
+ 
+export default ListaCitas;
